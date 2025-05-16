421b5e83eccbbd022430be25796cff1424fd8197
logan.puatu <loganjpuatu@gmail.com>
2025-05-16 16:29:07 -0600

    Simplify deployment pipeline and app configuration

--- src/quest_note/app.py
+++ src/quest_note/app.py
@@ -2,8 +2,8 @@ from flask import Flask
 
 app = Flask(
     __name__,
-    static_folder='../static',
-    template_folder='../templates'
+    static_folder='static',
+    template_folder='templates'
 )
 
 @app.route('/')
